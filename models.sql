31bc75aa3f3bdb27c21b045df0c30296afb4ffe3
Joseph Xu <josephxu@Josephs-MacBook-Pro.local>
2021-02-04 08:14:18 -0800

    first commit-build models and set relations

---
+++ models.sql
@@ -0,0 +1,115 @@
+CREATE TABLE IF NOT EXISTS member (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    signUpBy VARCHAR(255) NOT NULL DEFAULT '',
+    username VARCHAR(255) NOT NULL,
+    signUpInfo VARCHAR(255) NOT NULL DEFAULT '',
+    email VARCHAR(255) NOT NULL ,
+    password VARCHAR(255) NOT NULL ,
+    emailInfo VARCHAR(255) NOT NULL DEFAULT '',
+    phoneNumber VARCHAR(255) NOT NULL DEFAULT '',
+    bidYear smallint(4) NOT NULL DEFAULT 0,
+    bidMonth smallint(4) NOT NULL DEFAULT 0,
+    monIncome VARCHAR(255) NOT NULL DEFAULT '',
+    education VARCHAR(255) NOT NULL DEFAULT '',
+    gender CHAR(10) NOT NULL DEFAULT '',
+    favourites VARCHAR(255) NOT NULL DEFAULT '',
+    notification BOOLEAN NOT NULL DEFAULT FALSE,
+    realm VARCHAR(255) NOT NULL DEFAULT '',
+    emailVerified BOOLEAN NOT NULL DEFAULT FALSE,
+    verificationToken VARCHAR(255) DEFAULT NULL,
+    createdDate TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    status smallint(4) NOT NULL DEFAULT 1
+);
+
+
+CREATE TABLE IF NOT EXISTS reviewPlaning (
+    index INT PRIMARY KEY AUTO_INCREMENT,
+    asin VARCHAR(255) NOT NULL,
+    planReviewCount INT NOT NULL,
+    note VARCHAR(255) DEFAULT '',
+    operator VARCHAR(255)  DEFAULT '',
+    createdDate TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+
+CREATE TABLE IF NOT EXISTS favourite (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(255) UNIQUE NOT NULL  DEFAULT '',
+    description VARCHAR(255) DEFAULT '',
+    status BOOLEAN NOT NULL DEFAULT TRUE
+);
+
+CREATE TABLE IF NOT EXISTS Role (
+  id int(11) NOT NULL AUTO_INCREMENT,
+  name char(50) COLLATE utf8_unicode_ci DEFAULT NULL,
+  description char(100) COLLATE utf8_unicode_ci DEFAULT NULL,
+  created datetime DEFAULT NULL,
+  modified datetime DEFAULT NULL,
+  PRIMARY KEY (id),
+  UNIQUE KEY Role_id_uindex (id)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+CREATE TABLE IF NOT EXISTS RoleMapping (
+  id int(11) NOT NULL AUTO_INCREMENT,
+  principalType char(50) COLLATE utf8_unicode_ci NOT NULL,
+  principalId int(11) DEFAULT NULL,
+  roleId int(11) DEFAULT NULL,
+  PRIMARY KEY (id),
+  UNIQUE KEY AppRoleMapping_id_uindex (id)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+
+CREATE TABLE IF NOT EXISTS reviewPlaning (
+    index INT PRIMARY KEY AUTO_INCREMENT,
+    asin VARCHAR(255) NOT NULL,
+    planReviewCount INT NOT NULL,
+    note VARCHAR(255) DEFAULT '',
+    operator VARCHAR(255)  DEFAULT '',
+    createdDate TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+
+CREATE TABLE IF NOT EXISTS reviewTask (
+    id VARCHAR(255) NOT NULL PRIMARY KEY,
+    reviewPlaning VARCHAR(255) NOT NULL,
+    count VARCHAR(255) NOT NULL,
+    asin VARCHAR(255) NOT NULL,
+    url VARCHAR(255) DEFAULT '',
+);
+
+
+CREATE TABLE Buyer (
+  index INT PRIMARY KEY AUTO_INCREMENT,
+  buyerID VARCHAR(255) UNIQUE NOT NULL,
+  amzBuyerID VARCHAR(255) UNIQUE NOT NULL,
+  status BOOLEAN NOT NULL DEFAULT TRUE,
+  note VARCHAR(255) DEFAULT '',
+  mananger VARCHAR(255)  DEFAULT '',
+  description VARCHAR(255) DEFAULT '',
+  errorCode smallint(4) NOT NULL DEFAULT 0,
+  createdDate TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE Seller (
+  index INT PRIMARY KEY AUTO_INCREMENT,
+  sellerName VARCHAR(255) UNIQUE NOT NULL,
+  marketPlace VARCHAR(255) NOT NULL,
+  status BOOLEAN NOT NULL DEFAULT TRUE,
+  note VARCHAR(255) DEFAULT '',
+  mananger VARCHAR(255)  DEFAULT '',
+  description VARCHAR(255) DEFAULT '',
+  errorCode smallint(4) NOT NULL DEFAULT 0,
+  createdDate TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE Seller (
+  index INT PRIMARY KEY AUTO_INCREMENT,
+  sellerName VARCHAR(255) UNIQUE NOT NULL,
+  marketPlace VARCHAR(255) NOT NULL,
+  status BOOLEAN NOT NULL DEFAULT TRUE,
+  note VARCHAR(255) DEFAULT '',
+  mananger VARCHAR(255)  DEFAULT '',
+  description VARCHAR(255) DEFAULT '',
+  errorCode smallint(4) NOT NULL DEFAULT 0,
+  createdDate TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
