2e9483e0f98068bca1b17bc8f8b13143bfc0f4e0
Joseph Xu <josephxu@Josephs-MacBook-Pro.local>
2021-02-06 11:18:15 -0800

    first start after revert

--- models.sql
+++ models.sql
@@ -61,6 +61,7 @@ CREATE TABLE IF NOT EXISTS RoleMapping (
 
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 SELECT concat('DROP TABLE IF EXISTS `', table_name, '`;')
 FROM information_schema.tables
 WHERE table_schema = 'reviewsystem';
@@ -73,6 +74,72 @@ CREATE TABLE IF NOT EXISTS reviewPlaning (
     note VARCHAR(255) DEFAULT '',
     operator VARCHAR(255)  DEFAULT '',
     createdDate TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+=======
+
+CREATE TABLE IF NOT EXISTS Plan (
+    id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    planID VARCHAR(50) NOT NULL,
+    asin VARCHAR(50) NOT NULL,
+    planNumber INT NOT NULL,
+    finishNumber INT NOT NULL DEFAULT 0,
+    note VARCHAR(255) DEFAULT '',
+    operator VARCHAR(50)  DEFAULT '',
+    createdDate TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+
+CREATE TABLE IF NOT EXISTS Task (
+    id VARCHAR(50) NOT NULL PRIMARY KEY,
+    planID INT NOT NULL,
+    reviewProgress INT NOT NULL DEFAULT 1,
+    orderProgress INT NOT NULL DEFAULT 1,
+    note VARCHAR(255) ,
+    finish BOOLEAN DEFAULT FALSE,
+    status BOOLEAN DEFAULT TRUE,
+    updatedDate TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    FOREIGN KEY (planID) REFERENCES Plan(id),
+    FOREIGN KEY (reviewProgress) REFERENCES ReviewProgress(code),
+    FOREIGN KEY (orderProgress) REFERENCES OrderProgress(code)
+);
+
+CREATE TABLE IF NOT EXISTS Review (
+  id VARCHAR(50) NOT NULL PRIMARY KEY,
+  priority INT NOT NULL DEFAULT 1,
+  planID INT NOT NULL,
+  rating INT DEFAULT 5,
+  title VARCHAR(50) DEFAULT '',
+  content VARCHAR(255) DEFAULT '',
+  image VARCHAR(50)  DEFAULT '',
+  note VARCHAR(255) DEFAULT '',
+  message VARCHAR(255) DEFAULT '',
+  operator VARCHAR(255)  DEFAULT '',
+  status BOOLEAN DEFAULT TRUE,
+  updatedDate TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  FOREIGN KEY (planID) REFERENCES Plan(id),
+  FOREIGN KEY (priority) REFERENCES TaskPriority(code)
+);
+
+
+CREATE TABLE IF NOT EXISTS Order (
+  id VARCHAR(50) NOT NULL PRIMARY KEY,
+  planID INT NOT NULL,
+  sellerID INT ,
+  buyerID INT ,
+  orderNumber VARCHAR(255) DEFAULT '',
+  price INT ,
+  orderedDate TIMESTAMP ,
+  postDate TIMESTAMP ,
+  postBy VARCHAR(255) DEFAULT '',
+  isDelete BOOLEAN DEFAULT FALSE,
+  deleteDate TIMESTAMP ,
+  note VARCHAR(255) DEFAULT '',
+  message VARCHAR(255) DEFAULT '',
+  status BOOLEAN DEFAULT TRUE,
+  updatedDate TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  FOREIGN KEY (planID) REFERENCES Plan(id),
+  FOREIGN KEY (sellerID) REFERENCES Seller(id),
+  FOREIGN KEY (buyerID) REFERENCES Buyer(id)
+>>>>>>> parent of 125259c... modify primary key for models
 );
 
 
@@ -143,6 +210,7 @@ CREATE TABLE IF NOT EXISTS Order (
   isDelete BOOLEAN DEFAULT FALSE,
   deleteDate TIMESTAMP ,
   note VARCHAR(255) DEFAULT '',
+<<<<<<< HEAD
   message VARCHAR(255) DEFAULT '',
   status BOOLEAN DEFAULT TRUE,
   updatedDate TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
@@ -168,6 +236,9 @@ CREATE TABLE Buyer (
 =======
   mananger VARCHAR(50)  DEFAULT '',
 >>>>>>> parent of 125259c... modify primary key for models
+=======
+  mananger VARCHAR(50)  DEFAULT '',
+>>>>>>> parent of 125259c... modify primary key for models
   description VARCHAR(255) DEFAULT '',
   errorCode smallint(4) NOT NULL DEFAULT 0,
   createdDate TIMESTAMP DEFAULT CURRENT_TIMESTAMP
@@ -181,10 +252,14 @@ CREATE TABLE Seller (
   note VARCHAR(255) DEFAULT '',
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
   manager VARCHAR(50)  DEFAULT '',
 =======
   mananger VARCHAR(255)  DEFAULT '',
 >>>>>>> parent of d9759e4... restruct models
+=======
+  mananger VARCHAR(50)  DEFAULT '',
+>>>>>>> parent of 125259c... modify primary key for models
 =======
   mananger VARCHAR(50)  DEFAULT '',
 >>>>>>> parent of 125259c... modify primary key for models
@@ -195,6 +270,23 @@ CREATE TABLE Seller (
 
 <<<<<<< HEAD
 <<<<<<< HEAD
+=======
+CREATE TABLE IF NOT EXISTS SellerWarning (
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  reviewTaskID VARCHAR(255) UNIQUE NOT NULL,
+  reviewRating INT DEFAULT NULL,
+  reviewTitle VARCHAR(255) DEFAULT '',
+  reviewContent VARCHAR(255) DEFAULT '',
+  reiewImage VARCHAR(255)  DEFAULT '',
+  note VARCHAR(255) DEFAULT '',
+  operator VARCHAR(255)  DEFAULT '',
+  status BOOLEAN DEFAULT FALSE,
+  updatedDate TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  FOREIGN KEY (reviewTaskID) REFERENCES ReviewTask(id)
+);
+
+<<<<<<< HEAD
+>>>>>>> parent of 125259c... modify primary key for models
 =======
 CREATE TABLE IF NOT EXISTS SellerWarning (
   id INT PRIMARY KEY AUTO_INCREMENT,
@@ -251,6 +343,7 @@ CREATE TABLE IF NOT EXISTS Priority(
   chinese VARCHAR(50) NOT NULL ,
   korean VARCHAR(50) NOT NULL
 );
+<<<<<<< HEAD
 <<<<<<< HEAD
 
 CREATE TABLE IF NOT EXISTS Plan (
@@ -334,10 +427,15 @@ CREATE TABLE Seller (
 );
 >>>>>>> parent of d9759e4... restruct models
 =======
+=======
+>>>>>>> parent of 125259c... modify primary key for models
 [
   {code:1, english:"ORDER TASK CREATED DONE", chinese:"已创建订单任务", korean:"주문 작업 생성 완료"},
   {code:2, english:"ORDER NUMBER DONE", chinese:"已下单，待留评", korean:"주문 번호 완료"},
   {code:0, english:"ALL DONE", chinese:"已留评", korean:"모두 완료"},
   {code:-1, english:"DELETED", chinese:"已删除", korean:"삭제됨"},
 ]
+<<<<<<< HEAD
+>>>>>>> parent of 125259c... modify primary key for models
+=======
 >>>>>>> parent of 125259c... modify primary key for models
