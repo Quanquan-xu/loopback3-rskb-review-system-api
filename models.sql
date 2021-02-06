b44dcf9abc5f2f7c1a7a39c43794729114394c03
Joseph Xu <josephxu@Josephs-MacBook-Pro.local>
2021-02-06 11:47:00 -0800

    restruct models with primary key

--- models.sql
+++ models.sql
@@ -20,72 +20,20 @@ CREATE TABLE IF NOT EXISTS RoleMapping (
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 
-
-CREATE TABLE IF NOT EXISTS Plan (
-    id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
-    planID VARCHAR(50) NOT NULL,
-    asin VARCHAR(50) NOT NULL,
-    planNumber INT NOT NULL,
-    finishNumber INT NOT NULL DEFAULT 0,
-    note VARCHAR(255) DEFAULT '',
-    operator VARCHAR(50)  DEFAULT '',
-    createdDate TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
-);
+SELECT concat('DROP TABLE IF EXISTS `', table_name, '`;')
+FROM information_schema.tables
+WHERE table_schema = 'reviewsystem';
 
 
-CREATE TABLE IF NOT EXISTS Task (
-    id VARCHAR(50) NOT NULL PRIMARY KEY,
-    planID INT NOT NULL,
-    reviewProgress INT NOT NULL DEFAULT 1,
-    orderProgress INT NOT NULL DEFAULT 1,
-    note VARCHAR(255) ,
-    finish BOOLEAN DEFAULT FALSE,
-    status BOOLEAN DEFAULT TRUE,
-    updatedDate TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-    FOREIGN KEY (planID) REFERENCES Plan(id),
-    FOREIGN KEY (reviewProgress) REFERENCES ReviewProgress(code),
-    FOREIGN KEY (orderProgress) REFERENCES OrderProgress(code)
-);
-
-CREATE TABLE IF NOT EXISTS Review (
-  id VARCHAR(50) NOT NULL PRIMARY KEY,
-  priority INT NOT NULL DEFAULT 1,
-  planID INT NOT NULL,
-  rating INT DEFAULT 5,
-  title VARCHAR(50) DEFAULT '',
-  content VARCHAR(255) DEFAULT '',
-  image VARCHAR(50)  DEFAULT '',
-  note VARCHAR(255) DEFAULT '',
-  message VARCHAR(255) DEFAULT '',
-  operator VARCHAR(255)  DEFAULT '',
-  status BOOLEAN DEFAULT TRUE,
-  updatedDate TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  FOREIGN KEY (planID) REFERENCES Plan(id),
-  FOREIGN KEY (priority) REFERENCES TaskPriority(code)
-);
-
-
-CREATE TABLE IF NOT EXISTS Order (
-  id VARCHAR(50) NOT NULL PRIMARY KEY,
-  planID INT NOT NULL,
-  sellerID INT ,
-  buyerID INT ,
-  orderNumber VARCHAR(255) DEFAULT '',
-  price INT ,
-  orderedDate TIMESTAMP ,
-  postDate TIMESTAMP ,
-  postBy VARCHAR(255) DEFAULT '',
-  isDelete BOOLEAN DEFAULT FALSE,
-  deleteDate TIMESTAMP ,
-  note VARCHAR(255) DEFAULT '',
-  message VARCHAR(255) DEFAULT '',
-  status BOOLEAN DEFAULT TRUE,
-  updatedDate TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  FOREIGN KEY (planID) REFERENCES Plan(id),
-  FOREIGN KEY (sellerID) REFERENCES Seller(id),
-  FOREIGN KEY (buyerID) REFERENCES Buyer(id)
-);
-
+DROP TABLE IF EXISTS `Task`;
+DROP TABLE IF EXISTS `Order`;
+DROP TABLE IF EXISTS `Review`;
+DROP TABLE IF EXISTS `Plan`;
+DROP TABLE IF EXISTS `Buyer`;
+DROP TABLE IF EXISTS `OrderProgress`;
+DROP TABLE IF EXISTS `Priority`;
+DROP TABLE IF EXISTS `ReviewProgress`;
+DROP TABLE IF EXISTS `Seller`;
 
 CREATE TABLE IF NOT EXISTS Buyer (
   id INT PRIMARY KEY AUTO_INCREMENT,
@@ -93,7 +41,7 @@ CREATE TABLE IF NOT EXISTS Buyer (
   amzBuyerID VARCHAR(100) NOT NULL,
   status BOOLEAN  DEFAULT TRUE,
   note VARCHAR(255) DEFAULT '',
-  mananger VARCHAR(50)  DEFAULT '',
+  manager VARCHAR(50)  DEFAULT '',
   description VARCHAR(255) DEFAULT '',
   errorCode smallint(4) DEFAULT NULL,
   createdDate TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
@@ -106,59 +54,13 @@ CREATE TABLE IF NOT EXISTS Seller (
   marketPlace VARCHAR(50) NOT NULL,
   status BOOLEAN DEFAULT TRUE,
   note VARCHAR(255) DEFAULT '',
-  mananger VARCHAR(50)  DEFAULT '',
+  manager VARCHAR(50)  DEFAULT '',
   description VARCHAR(255) DEFAULT '',
   errorCode smallint(4) DEFAULT NULL,
   createdDate TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
   updatedDate TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 );
 
-CREATE TABLE IF NOT EXISTS SellerWarning (
-  id INT PRIMARY KEY AUTO_INCREMENT,
-  reviewTaskID VARCHAR(255) UNIQUE NOT NULL,
-  reviewRating INT DEFAULT NULL,
-  reviewTitle VARCHAR(255) DEFAULT '',
-  reviewContent VARCHAR(255) DEFAULT '',
-  reiewImage VARCHAR(255)  DEFAULT '',
-  note VARCHAR(255) DEFAULT '',
-  operator VARCHAR(255)  DEFAULT '',
-  status BOOLEAN DEFAULT FALSE,
-  updatedDate TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  FOREIGN KEY (reviewTaskID) REFERENCES ReviewTask(id)
-);
-
-
-
-
-
-
-
-CREATE TABLE IF NOT EXISTS ReviewProgress(
-  id INT PRIMARY KEY AUTO_INCREMENT,
-  code INT UNIQUE NOT NULL,
-  english VARCHAR(50) NOT NULL ,
-  chinese VARCHAR(50) NOT NULL ,
-  korean VARCHAR(50) NOT NULL
-);
-[
-  {code:1, english:"REVIEW TASK CREATED DONE", chinese:"已创建评论任务", korean:"작성된 작업 검토 완료"},
-  {code:2, english:"REVIEW WRITED DONE", chinese:"已经写评论", korean:"작성된 검토 완료"},
-]
-CREATE TABLE IF NOT EXISTS OrderProgress(
-  id INT PRIMARY KEY AUTO_INCREMENT,
-  code INT UNIQUE NOT NULL,
-  english VARCHAR(50) NOT NULL ,
-  chinese VARCHAR(50) NOT NULL ,
-  korean VARCHAR(50) NOT NULL
-);
-
-[
-  {code:1, english:"ORDER TASK CREATED DONE", chinese:"已创建订单任务", korean:"주문 작업 생성 완료"},
-  {code:2, english:"ORDER NUMBER DONE", chinese:"已下单，待留评", korean:"주문 번호 완료"},
-  {code:3, english:"ORDER REVIEW DONE", chinese:"已留评", korean:"주문 검토 완료"},
-  {code:4, english:"ALL DONE", chinese:"已留评", korean:"모두 완료"},
-  {code:5, english:"DELETED", chinese:"被删除", korean:"삭제됨"},
-]
 
 CREATE TABLE IF NOT EXISTS Priority(
   id INT PRIMARY KEY AUTO_INCREMENT,
@@ -167,9 +69,85 @@ CREATE TABLE IF NOT EXISTS Priority(
   chinese VARCHAR(50) NOT NULL ,
   korean VARCHAR(50) NOT NULL
 );
-[
-  {code:1, english:"ORDER TASK CREATED DONE", chinese:"已创建订单任务", korean:"주문 작업 생성 완료"},
-  {code:2, english:"ORDER NUMBER DONE", chinese:"已下单，待留评", korean:"주문 번호 완료"},
-  {code:0, english:"ALL DONE", chinese:"已留评", korean:"모두 완료"},
-  {code:-1, english:"DELETED", chinese:"已删除", korean:"삭제됨"},
-]
+
+CREATE TABLE IF NOT EXISTS OrderProgress(
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  code INT UNIQUE NOT NULL,
+  english VARCHAR(50) NOT NULL ,
+  chinese VARCHAR(50) NOT NULL ,
+  korean VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS ReviewProgress(
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  code INT UNIQUE NOT NULL,
+  english VARCHAR(50) NOT NULL ,
+  chinese VARCHAR(50) NOT NULL ,
+  korean VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS Plan (
+    id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    planID VARCHAR(50) NOT NULL,
+    asin VARCHAR(50) NOT NULL,
+    planNumber INT NOT NULL,
+    finishNumber INT NOT NULL DEFAULT 0,
+    note VARCHAR(255) DEFAULT '',
+    operator VARCHAR(50)  DEFAULT '',
+    createdDate TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE IF NOT EXISTS Task (
+    id VARCHAR(50) NOT NULL PRIMARY KEY,
+    plan INT NOT NULL,
+    reviewProgress INT NOT NULL DEFAULT 1,
+    orderProgress INT NOT NULL DEFAULT 1,
+    note VARCHAR(255) ,
+    finish BOOLEAN DEFAULT FALSE,
+    status BOOLEAN DEFAULT TRUE,
+    updatedDate TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    FOREIGN KEY (plan) REFERENCES Plan(id),
+    FOREIGN KEY (reviewProgress) REFERENCES ReviewProgress(id),
+    FOREIGN KEY (orderProgress) REFERENCES OrderProgress(id)
+);
+
+CREATE TABLE IF NOT EXISTS Review (
+  id VARCHAR(50) NOT NULL PRIMARY KEY,
+  priority INT NOT NULL DEFAULT 1,
+  plan INT NOT NULL,
+  rating INT DEFAULT 5,
+  title VARCHAR(50) DEFAULT '',
+  content VARCHAR(255) DEFAULT '',
+  image VARCHAR(50)  DEFAULT '',
+  note VARCHAR(255) DEFAULT '',
+  message VARCHAR(255) DEFAULT '',
+  operator VARCHAR(255)  DEFAULT '',
+  status BOOLEAN DEFAULT TRUE,
+  updatedDate TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  FOREIGN KEY (plan) REFERENCES Plan(id),
+  FOREIGN KEY (priority) REFERENCES Priority(id)
+);
+
+CREATE TABLE IF NOT EXISTS `Order` (
+  id VARCHAR(50) NOT NULL PRIMARY KEY,
+  plan INT NOT NULL,
+  seller INT,
+  buyer INT ,
+  orderNumber VARCHAR(255) DEFAULT NULL,
+  price INT DEFAULT NULL,
+  orderedDate TIMESTAMP DEFAULT NULL,
+  postDate TIMESTAMP DEFAULT NULL,
+  postBy VARCHAR(255) DEFAULT NULL,
+  diabled BOOLEAN DEFAULT FALSE,
+  diabledDate TIMESTAMP DEFAULT NULL,
+  note VARCHAR(255) DEFAULT '',
+  message VARCHAR(255) DEFAULT '',
+  status BOOLEAN DEFAULT TRUE,
+  updatedDate TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  FOREIGN KEY (plan) REFERENCES Plan(id),
+  FOREIGN KEY (seller) REFERENCES Seller(id),
+  FOREIGN KEY (buyer) REFERENCES Buyer(id)
+);
+
+
+
